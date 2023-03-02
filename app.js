f725195c9f6abd609eebb8b16db197b50e77e4f4
JohannesSloth <joha218s@stud.kea.dk>
2023-03-02 17:19:54 +0100

    test if deploying

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 const express = require("express");
 const app = express();
-const mime = require('mime');
 
-app.use(express.static("public", {
-  setHeaders: (res, path) => {
-    res.setHeader('Content-Type', mime.getType(path));
-  }
-}));
+app.use(express.static("public"))
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/frontpage/frontpage.html")
